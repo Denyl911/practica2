75a122631e152435afd8aa4bb641010c1978ff0e
Denyl911 <imdenyl@gmail.com>
2023-02-06 22:23:16 -0600

    first commit

---
+++ src/main/java/com/mycompany/practica2/Practica2.java
@@ -0,0 +1,15 @@
+package com.mycompany.practica2;
+
+/**
+ *
+ * @author denyl
+ */
+public class Practica2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+        new Frame().setVisible(true);
+
+    }
+}
